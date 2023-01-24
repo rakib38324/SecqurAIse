707827946f387e92e9ed523937623d9f0386586f
rakib38324 <rakib3832@gmail.com>
2023-01-24 20:28:12 +0600

    Commit the style

--- src/Hooks/useDataList.js
+++ src/Hooks/useDataList.js
@@ -1,60 +1,65 @@
 import {
+  equalTo,
   get,
   getDatabase,
+  onValue,
+  orderByChild,
   orderByKey,
   query,
   ref,
+  startAt,
 
 } from "firebase/database";
 import { useEffect, useState } from "react";
 
 
- const useDataList = ()=> {
+const useDataList = () => {
 
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(false);
-    const [data, setData] = useState([]);
-    useEffect(() => {
-        async function fetchData() {
-            const db = getDatabase();
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    async function fetchData() {
+      const db = getDatabase();
 
-            const databaseRef = ref(db, "data");
+      const databaseRef = ref(db, "data");
 
-            const dataQuery = query(
-                databaseRef,
-                
-                orderByKey(),
-            );
+      const dataQuery = query(
+        databaseRef,
+        // startAt("Bangalore")
 
-            try {
-                setError(false);
-                setLoading(true);
-                // request firebase database
-                const snapshot = await get(dataQuery);
-                setLoading(false);
-                if (snapshot.exists()) {
-                  setData((prevData) => {
-                    return [...prevData, ...Object.values(snapshot.val())];
-                  });
-                } else {
-                  // setHasMore(false);
-                }
-              } catch (err) {
-                console.log(err);
-                setLoading(false);
-                setError(true);
-              }
+      );
 
-        }
+  try {
+    setError(false);
+    setLoading(true);
+    // request firebase database
+    const snapshot = await get(dataQuery);
+    setLoading(false);
+    if (snapshot.exists()) {
+      setData((prevData) => {
+        return [...prevData, ...Object.values(snapshot.val())];
+      });
+    } else {
+      // setHasMore(false);
+      console.log("Nothing")
+    }
+  } catch (err) {
+    console.log(err);
+    setLoading(false);
+    setError(true);
+  }
 
-        fetchData();
-    }, []);
+}
 
-    return {
-      loading,
-      error,
-      data,
-      
-    };
+fetchData();
+  }, []);
+
+return {
+  loading,
+  error,
+  data,
+
+};
 }
 export default useDataList;
--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,11 +1,17 @@
 import { child, get, getDatabase, ref } from 'firebase/database';
 import React, { useState } from 'react';
+import { FaFilter } from "react-icons/fa";
 import useDataList from '../../Hooks/useDataList';
+import "react-datepicker/dist/react-datepicker.css";
+import DatePicker from "react-datepicker";
 
 
 const Home = () => {
   const { data } = useDataList();
 
+
+  const [startDate, setStartDate] = useState(new Date());
+console.log(JSON.stringify(startDate).slice(1,11))
   const [info, setInfo] = useState({})
 
   console.log(info)
@@ -24,6 +30,16 @@ const Home = () => {
 
   }
 
+  const handleFilter = (event) => {
+    console.log("User Selected Value - ", event.target.value)
+
+  }
+
+  
+
+
+
+
   return (
     <div className=' lg:grid lg:grid-cols-12'>
 
@@ -39,52 +55,52 @@ const Home = () => {
       <div className=' lg:col-span-7 lg:sticky lg:top-16 lg:h-[500px]'>
 
         <p className='text-center text-2xl mb-5 font-bold'> {info.Gender}</p>
-        
+
         {
-          info.ID  ?
-          <div className='lg:grid lg:grid-cols-2'>
+          info.ID ?
+            <div className='lg:grid lg:grid-cols-2'>
 
-          <div className='text-center lg:text-start'>
-            <p className='text-2xl font-bold '>{info.ID}</p>
-            <p className='text-2xl font-bold mb-3'>Person detected</p>
-            
-            <div className="overflow-x-auto">
-              <table className="ml-4 lg:ml-0 table border-none ">
-                
-                
-                <tbody>
-                 
-                  <tr>
-                    <td className='border-0 p-0 pr-3 text-2xl'>Name</td>
-                    <td className='border-0 p-0 text-2xl'>: {info.Name}</td>
-                  </tr>
-                 
-                  <tr>
-                    <td className='border-0 p-0 pr-3 text-2xl'>Location</td>
-                    <td className='border-0 p-0 text-2xl'>: {info.Location}</td>
-                  </tr>
-                
-                  <tr>
-                    <td className='border-0 p-0 pr-3 text-2xl'>Date</td>
-                    <td className='border-0 p-0 text-2xl'>: {info.Date}</td>
-                  </tr>
-                  <tr>
-                    <td className='border-0 p-0 pr-3 text-2xl'>Time</td>
-                    <td className='border-0 p-0 text-2xl'>: {info.Time}</td>
-                  </tr>
-                
-                </tbody>
-              </table>
+              <div className='text-center lg:text-start'>
+                <p className='text-2xl font-bold '>{info.ID}</p>
+                <p className='text-2xl font-bold mb-3'>Person detected</p>
+
+                <div className="overflow-x-auto">
+                  <table className="ml-4 lg:ml-0 table border-none ">
+
+
+                    <tbody>
+
+                      <tr>
+                        <td className='border-0 p-0 pr-3 text-2xl'>Name</td>
+                        <td className='border-0 p-0 text-2xl'>: {info.Name}</td>
+                      </tr>
+
+                      <tr>
+                        <td className='border-0 p-0 pr-3 text-2xl'>Location</td>
+                        <td className='border-0 p-0 text-2xl'>: {info.Location}</td>
+                      </tr>
+
+                      <tr>
+                        <td className='border-0 p-0 pr-3 text-2xl'>Date</td>
+                        <td className='border-0 p-0 text-2xl'>: {info.Date}</td>
+                      </tr>
+                      <tr>
+                        <td className='border-0 p-0 pr-3 text-2xl'>Time</td>
+                        <td className='border-0 p-0 text-2xl'>: {info.Time}</td>
+                      </tr>
+
+                    </tbody>
+                  </table>
+                </div>
+
+                <p className='text-xl text-center lg:text-start lg:w-1/2 mt-5 font-semibold'>Description: {info.Details}</p>
+              </div>
+              <div >
+                <img className='mx-auto my-5 lg:w-3/4 h-96' src={info.Image} alt='profile'></img>
+              </div>
             </div>
-
-            <p className='text-xl text-center lg:text-start lg:w-1/2 mt-5 font-semibold'>Description: {info.Details}</p>
-          </div>
-          <div >
-            <img className='mx-auto my-5 lg:w-3/4 h-96' src={info.Image} alt='profile'></img>
-          </div>
-        </div>
-        :
-        <p className='text-center text-2xl font-semibold pt-5'>Data not Found. Please Click any Event ---></p>
+            :
+            <p className='text-center text-2xl font-semibold pt-5'>Data not Found. Please Click any Event ---> </p>
         }
 
 
@@ -95,9 +111,41 @@ const Home = () => {
       <div className=' flex flex-col justify-center lg:col-span-4 border-4'>
 
         <div>
-          <div className='flex justify-between '>
+          <div className='flex justify-between my-2 px-2'>
             <p className='text-2xl'>Event</p>
-            <p className='text-xl'>filter</p>
+            <p className='text-xl'>
+              {/* The button to open modal */}
+              <label htmlFor="my-modal" className="text-4xl "><FaFilter></FaFilter></label>
+
+              {/* Put this part before </body> tag */}
+              <input type="checkbox" id="my-modal" className="modal-toggle" />
+              <div className="modal">
+                <div className="modal-box w-11/12 max-w-xl">
+                  <h3 className="font-bold text-lg">Please Select</h3>
+                  
+                  <div onChange={handleFilter}>
+                    <select className="select select-bordered w-full my-2">
+                      <option disabled selected>Location</option>
+                      <option>Chennai</option>
+                      <option>Hyderabad </option>
+                      <option>Bangalore</option>
+                    </select>
+                    
+                    <select className="select select-bordered w-full my-2">
+                      <option disabled selected>Gender</option>
+                      <option>Male</option>
+                      <option>Female</option>
+                    </select>
+                    
+                    <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} />
+                  </div>
+
+                  <div className="modal-action">
+                    <label htmlFor="my-modal" className="bg-base-300 p-3 rounded-md cursor-pointer">OK</label>
+                  </div>
+                </div>
+              </div>
+            </p>
           </div>
           {
             data.map((info) => (
